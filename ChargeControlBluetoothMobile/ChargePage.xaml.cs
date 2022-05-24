093548cff91a9226901cf282595f6488ea712357
DenysKlishchov <89396622+DenysKlishchov@users.noreply.github.com>
2022-05-24 12:57:20 +0300

    Добавьте файлы проекта.

---
+++ ChargeControlBluetoothMobile/ChargePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChargeControlBluetoothMobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChargePage : ContentPage
+    {
+        public ChargePage()
+        {
+            InitializeComponent();
+        }
+        private IDevice conDevice;
+        private async void GetCharge()
+        {
+            IService service;
+            conDevice = (IDevice)BindingContext;
+            try
+            {
+                service = await conDevice.GetServiceAsync(Guid.Parse("0000180F-0000-1000-8000-00805F9B34FB"));
+            }
+            catch (Exception ex)
+            {
+               await DisplayAlert("Error", ex.Message,"Ok");
+            }
+            var characteristics = await service.GetCharacteristicsAsync();
+            var characteristic = characteristics[0];
+
+            ChargeText.Text = characteristic.StringValue;
+        }
+
+        private void GetCharge_btn(object sender, EventArgs e)
+        {
+            GetCharge();
+        }
+    }
+}
---
+++ ChargeControlBluetoothMobile/MainPage.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ChargeControlBluetoothMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        IBluetoothLE ble;
+        IAdapter adapter;
+        ObservableCollection<IDevice> deviceList;
+
+        private IDevice device;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ble = CrossBluetoothLE.Current;
+            adapter = CrossBluetoothLE.Current.Adapter;
+            deviceList = new ObservableCollection<IDevice>();
+
+            DevicesList.ItemsSource = deviceList;
+        }
+
+
+
+        private async void ScanDevice(object sender, EventArgs e)
+        {
+
+            if (ble.State == BluetoothState.Off)
+            {
+                await DisplayAlert("attention", "bluetooth off", "ok");
+            }
+            else
+            {
+                deviceList.Clear();
+
+                adapter.ScanTimeout = 10000;
+                adapter.ScanMode = ScanMode.Balanced;
+
+
+                adapter.DeviceDiscovered += (obj, a) =>
+                {
+                    if (!deviceList.Contains(a.Device))
+                        deviceList.Add(a.Device);
+                };
+
+                await adapter.StartScanningForDevicesAsync();
+
+            }
+        }
+
+        private async void DevicesList_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            device = DevicesList.SelectedItem as IDevice;
+
+            var result = await DisplayAlert("WARNING", "Do you wish to connect to this device?", "Connect", "Cancel");
+
+            if (!result)
+                return;
+
+            //Stop Scanner
+            await adapter.StopScanningForDevicesAsync();
+
+            try
+            {
+                await adapter.ConnectToDeviceAsync(device);
+
+                await DisplayAlert("Connected", "Status:" + device.State, "OK");
+
+                await Navigation.PushAsync(new ChargePage { BindingContext = device });
+
+            }
+            catch (DeviceConnectionException ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+
+        }
+
+       
+    }
+}
